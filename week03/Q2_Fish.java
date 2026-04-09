846fe0b118ccee55dab88aee69fcbbd2cc3b30f2
kyle0125 <c112181124@nkust.edu.tw>
2026-04-09 11:51:36 +0800

    完成第3週作業

--- week03/Q2_Fish.java
+++ week03/Q2_Fish.java
@@ -1,18 +1,19 @@
-public class Q2_fish{
-    String name;
-    double weight;
+public class Q2_fish {
 
-    public void displayinf(){
-         System.out.println("This fish's name is:" + name + "Weight: "+ wieght +"KG");
-    }
+String name;
+double weight;
 
-    public static void main(String [] args){
-        Q2_fish myfish = new Q2_fish();
-        myfish.name = "Black fish";
-        myfish.weight =250.5;
-
-        myfish.displayinf();
-
-    }
-
+public void displayInfo() {
+System.out.println("這隻魚的名字是：" + name + "，重量是：" + weight + "公斤");
 }
+public static void main(String[] args) {
+
+Q2_fish myFish = new Q2_fish();
+
+myFish.name = "黑鮪魚";
+myFish.weight = 250.5;
+
+myFish.displayInfo();
+}
+} 
+ 
